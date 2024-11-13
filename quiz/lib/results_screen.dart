b891fc20b8b7e11a7d4c4b268e7c9394838865b2
ahmed wajih ben smida <ahmed.wajih.ben.smida@gmail.com>
2024-11-13 22:21:38 +0100

    some modif

--- quiz/lib/results_screen.dart
+++ quiz/lib/results_screen.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 
-class ResultsScreen  extends StatelessWidget {
+class ResultsScreen extends StatelessWidget {
+  const ResultsScreen({super.key});
 
   @override
-  Widget build (BuildContext context) {
-    0
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Results'),
+      ),
+      body: const Center(
+        child: Text('Results Screen'),
+      ),
+    );
   }
 }
